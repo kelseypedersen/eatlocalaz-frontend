064bdd9a0373122093575c1a4b0807963ebc0c0a
Kelsey Pedersen <kelsey@leadgenius.com>
2016-01-23 18:41:23 -0800

    Connected with API - working MVP

--- www/js/app.js
+++ www/js/app.js
@@ -6,99 +6,96 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+    .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+        $stateProvider
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  })
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
 
-  .state('app.letters', {
-    url: '/letters',
-    abstract: true,
-    templateUrl: 'templates/letters.html',
-    controller: 'LettersCtrl'
-  })
+            .state('app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/search.html'
+                    }
+                }
+            })
 
-  .state('app.letter', {
-    url: '/:letter',
-    abstract: true,
-    templateUrl: 'templates/letter.html',
-    controller: 'LetterCtrl'
-  })
+            .state('app.browse', {
+                url: '/browse',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/browse.html'
+                    }
+                }
+            })
+            // Changed
+            .state('app.letters', {
+                url: '/letters',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/letters.html',
+                        controller: 'LettersCtrl'
+                    }
+                }
+            }).state('app.results', {
+                url: '/:cuisine',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/results.html',
+                        controller: 'ResultsCtrl'
+                    }
+                }
+            })
+            .state('app.single', {
+                url: '/playlists/:playlistId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlist.html',
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            }).state('app.letter', {
+                url: '/letters/:letter',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/letter.html',
+                        controller: 'LetterCtrl'
+                    }
+                }
+            }).state('app.details', {
+                url: '/details/:yelp_id',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/details.html',
+                        controller: 'ResultsCtrl'
+                    }
+                }
+            });
 
-  .state('app.results', {
-    url: '/:letter/results',
-    abstract: true,
-    templateUrl: 'templates/results.html',
-    controller: 'ResultsCtrl'
-  })
 
-  .state('app.details', {
-    url: '/:yelp_id',
-    abstract: true,
-    templateUrl: 'templates/details.html',
-    controller: 'DetailsCtrl'
-  })
+        // if none of the above states are matched, use this as the fallback
+        //$urlRouterProvider.otherwise('/app/playlists');
+         $urlRouterProvider.otherwise('/app/letters');
 
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,99 +1,155 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
 
-// Example for 1-Letters, 2-Letter, 3-Results //
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    //Example for 1-Letters, 2-Letter, 3-Results //
+//.controller('PlaylistsCtrl', function($scope) {
+//  $scope.playlists = [
+//    { title: 'Reggae', id: 1 },
+//    { title: 'Chill', id: 2 },
+//    { title: 'Dubstep', id: 3 },
+//    { title: 'Indie', id: 4 },
+//    { title: 'Rap', id: 5 },
+//    { title: 'Cowbell', id: 6 }
+//  ];
+//})
 
 // Page 1 //
-.controller('LettersCtrl', function($scope) {
-  $scope.letters = [
-    { title: 'A', id: 1 },
-    { title: 'B', id: 2 },
-    { title: 'C', id: 3 },
-    { title: 'D', id: 4 },
-    { title: 'E', id: 5 },
-    { title: 'F', id: 6 }
-  ];
-})
+    .controller('LettersCtrl', function ($scope) {
+        $scope.letters = [
+            {title: 'A', id: 1},
+            {title: 'B', id: 2},
+            {title: 'C', id: 3},
+            {title: 'D', id: 4},
+            {title: 'E', id: 5},
+            {title: 'F', id: 6}
+        ];
+    })
+
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
+    })
 
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+    .controller('LetterCtrl', function ($scope, $stateParams) {
+        $scope.header = $stateParams.letter;
 
-// Page 2 //
-.controller('LetterCtrl', function($scope, $stateParams) {
-  $scope.letter = [
-    // Need to add all the cuisines for the specific letter here
-    // Response will be coming from this repo
-    // Include all letters in file and query the object in file
-  ]
-});
+        $scope.letter = function () {
 
-// Page 3 //
-.controller('ResultsCtrl', function($scope, $stateParams) {
-  $scope.results = [
-    // Need to add all the cuisines for the specific letter here
-    // Response will be coming from the Yelp Search API for all_categories_list
-    // Params passed will the cuisine type
-  ]
-});
+            var categories = {"A": ["Afghan", "African", "American"], "B": ["BBQ"]};
 
-.controller('DetailsCtrl', function($scope, $stateParams) {
-  $scope.details = [
-    // Add details for one Yelp restaurant here
-    // Response will be coming from the Yelp Business API
-    // Params passed will be the Yelp ID key
-  ]
-});
+            var letter = $stateParams.letter;
+
+            $scope.results = categories[letter];
+
+            return $scope.results;
+        };
+
+        $scope.results = $scope.letter();
+
+    })
+
+    // Page 3 //
+    .controller('ResultsCtrl', function ($scope, $stateParams, $http, Data) {
+        $scope.Data = Data;
+
+        var cuisine = $stateParams.cuisine;
+
+        $http({
+            method: 'GET',
+            url: 'http://eat-local-a-z.herokuapp.com/api/' + cuisine + '/37/104',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            data: {cuisine: 'cuisine'}
+        }).success(function (response) {
+            console.log(response);
+            $scope.response = response;
+
+            $scope.yelp_id = response.id;
+
+        }).error(function (err) {
+            // handle error
+        });
+
+        $scope.clickEvent = function (restaurant) {
+            console.log(restaurant);
+            $scope.Data.display_phone = restaurant.id;
+            $scope.Data.name = restaurant.name;
+            $scope.Data.rating = restaurant.rating;
+            $scope.Data.location = restaurant.location;
+            $scope.Data.rating_img_url_small = restaurant.rating_img_url_small;
+            $scope.Data.image_url = restaurant.image_url;
+            $scope.Data.display_phone = restaurant.display_phone;
+        };
+    })
+
+
+    .controller('DetailsCtrl', function ($scope, $stateParams, Data) {
+        $scope.Data = Data;
+        console.log($scope.response);
+
+        //console.log($stateParams.response);
+
+        console.log($stateParams);
+        $scope.details = [
+            // Add details for one Yelp restaurant here
+            // Response will be coming from the Yelp Business API
+            // Params passed will be the Yelp ID key
+        ]
+    })
+
+    .factory('Data', function () {
+        return {
+            order: '',
+            id: '',
+            rating: '',
+            location: '',
+            rating_img_url_small: '',
+            image_url: '',
+            display_phone: ''
+        }
+    });
+
+
 
 
 
